48b87865ea631ad7aceeb547b68edb37a24f0204
akadir8 <bickiabdulkadir12@gmail.com>
2024-03-12 22:11:50 +0300

    Basic Login Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.loginImg}>
-        <Image src="/assets/loginImg.svg" width={341} height={535} alt="image"/>
-      </div>
-      <div className={styles.loginBackground}>
-        <Image src="/assets/loginBackground.svg" width={649} height={534} alt="background" />
-      </div>
       <div className={styles.loginForm}>
-        {/* Form alanınızı burada oluşturabilirsiniz */}
+        <h3>Web Application</h3>
         <form>
           <label htmlFor="email">Email</label>
           <input type="email" id="email" name="email" required />
@@ -22,6 +17,11 @@ export default function Home() {
           <button type="submit">Sign in</button>
         </form>
       </div>
+      </div>
+      <div className={styles.loginBackground}>
+        <Image src="/assets/octopos.png" width={700} height={400} alt="background" />
+      </div>
+      
     </div>
   );
 }
